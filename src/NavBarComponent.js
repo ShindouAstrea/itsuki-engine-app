3f927a876e9cbc31a3bb3295078e125bb15b32ed
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-08-16 16:05:57 -0400

    Features components

---
+++ src/NavBarComponent.js
@@ -0,0 +1,8 @@
+import {Text} from 'react-native';
+function NabBarComponent() {
+  return (
+   <Text> NabBar</Text>
+    
+  );
+}
+export default NabBarComponent;
---
+++ src/Views/SettingsView.js
@@ -0,0 +1,8 @@
+import {View,Text} from 'react-native';
+export default function SettingsView(){
+    return(
+        <View>
+             <Text> Settings</Text>
+        </View>
+    )
+}
