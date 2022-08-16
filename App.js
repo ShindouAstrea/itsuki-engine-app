b632356595f105e19d676d5a0dcc658b5d681944
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-08-16 16:05:28 -0400

    Navigation dependencies

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeView from './src/Views/HomeView';
+import HistoryView from './src/Views/HistoryView';
+import MoreResponseView from './src/Views/MoreResponseView';
+import SettingsView from './src/Views/SettingsView';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+<>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#261447',
+        },
+        headerTintColor: '#ffff',
+        
+        headerTitleAlign: 'center',
+      }}>
+        <Stack.Screen name="Home" component={HomeView} options={{
+        title: 'Inicio'}}/>
+        <Stack.Screen name="Settings" component={SettingsView} />
+        <Stack.Screen name="More" component={MoreResponseView} />
+        <Stack.Screen name="History" component={HistoryView} />
+      </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
