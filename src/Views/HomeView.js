a4383f7fa7225d23999b7760bb6914e108504edb
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-09-20 14:37:26 -0400

    Use of TRACE MOE for searchs

--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -1,7 +1,6 @@
 import React from 'react';
-
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View,Text,TouchableHighlight,Image,Button,Alert} from 'react-native';
+import { StyleSheet, View,Text,TouchableHighlight,Image} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import {loadImageFromGallery} from '../ImagePick' ;
 import * as Clipboard from 'expo-clipboard';
@@ -11,9 +10,24 @@ function HomeView({navigation}){
     const clip = 'im a text 4';
     const [visibleButton, setButtonVisible] = React.useState(false);
     const [pic, setPic]= React.useState(photo);
-
+    const [resultado,setResultado] = React.useState([]);
+    
     const [copiedText, setCopiedText] = React.useState('');
-
+    const listAnimes=(item) => {
+        console.log("Nombre del Anime: " + item.filename)
+       console.log("Porcentaje de coincidencia: "+parseFloat(item.similarity)*100+"%");
+    }
+    const searchAnime= async() =>{
+        await fetch(
+            `https://api.trace.moe/search?url=${encodeURIComponent(
+              "https://firebasestorage.googleapis.com/v0/b/itsuki-engine.appspot.com/o/fed879f43090492ec9e8350c848614a2.jpg?alt=media&token=b1903063-ddfe-45be-be6d-a047fdd90aeb"
+            )}`)
+       .then((response)=>response.json())
+       .then((response)=>
+        setResultado((response.result)))
+        .then(()=>resultado.forEach(listAnimes))
+       .catch((error)=>console.log( `error : ${error}`))
+    };
     const copyToClipboard = async () => {
       await Clipboard.setStringAsync(clip);
       };
@@ -44,7 +58,11 @@ function HomeView({navigation}){
                 </TouchableHighlight>
               {
                 visibleButton &&(
-                    <TouchableOpacity onPress={()=>navigation.navigate('Response',{photo:{pic}})}>
+                    <TouchableOpacity onPress={()=>{
+                        searchAnime();
+                        navigation.navigate('Response',
+                        {photo:{pic}})
+                        }}>
                             <View style={styles.UploadButton}>
                                 <Text style={styles.TextButton}>Realizar busqueda</Text>
                             </View>
@@ -52,14 +70,6 @@ function HomeView({navigation}){
 
                 )
               }
-                <TouchableOpacity onPress={copyToClipboard}>
-                    <Text>{clip}</Text>
-                </TouchableOpacity>
-                <Button title="View copied text" onPress={fetchCopiedText} />
-               
-                <Text style={styles.copiedText}>{copiedText}</Text>   
-               
-                
         <StatusBar style="auto" />
         </View>
         
--- src/Views/ResponseView.js
+++ src/Views/ResponseView.js
@@ -2,38 +2,16 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View,Text} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { fileToBlob } from '../ImagePick';
 
 function ResponseView({navigation,route}){
     const {photo} = route.params ;
-    const body = new FormData();
     const pic =  JSON.stringify(photo).slice(8,JSON.stringify(photo).length -2) ;
   
-    React.useEffect(()=>{
-        const searchAnime= async() =>{
-            const picToUpload =  await fileToBlob(pic);
-            console.log(picToUpload);
-            body.append("image", picToUpload);
-            await fetch("https://api.trace.moe/search",{
-            method: 'POST',
-            body: body,
-           })
-           .then((response)=>response.json())
-           .then((response)=>console.log(JSON.stringify(response)))
-           .catch((error)=>console.log( `error : ${error}`))
-        }
-        searchAnime() ;
-    },[])
     return(
         <View style={styles.Container}>
                 <View style={{marginBottom:40}}>
                     <Image source={{uri: pic}} style={{width:300, height:300}}/>
                 </View>
-                <TouchableOpacity onPress={()=>navigation.navigate('MoreResponse')}>
-                    <View style={styles.UploadButton}>
-                        <Text style={styles.TextButton}>Ver más resultados</Text>
-                    </View>
-                </TouchableOpacity>
         <StatusBar style="auto" />
         </View>
         
