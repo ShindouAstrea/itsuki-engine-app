5d5f41202f5769ab7eb3cd793f07a098fbd0ef24
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-09-23 18:52:36 -0400

    Config ts for firebase

---
+++ src/database/firebaseConfig.tsx
@@ -0,0 +1,12 @@
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ export default {
+  apiKey: "AIzaSyCK9Eo3jBZezJ7z2MT7l-jpAtB6eNpCbi4",
+  authDomain: "itsuki-engine.firebaseapp.com",
+  projectId: "itsuki-engine",
+  storageBucket: "itsuki-engine.appspot.com",
+  messagingSenderId: "348429015261",
+  appId: "1:348429015261:web:4bbddec27c7e690cb26334",
+  measurementId: "G-EF0RTTMVXD"
+};
