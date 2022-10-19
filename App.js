0f09ea5cc75695e6c347ccbaf6566bf31a19e953
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-10-19 15:14:25 -0400

    fix: colors

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import * as React from 'react';
+
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer  } from '@react-navigation/native';
+
 import HistoryView from './src/Components/Views/HistoryView';
 import SettingsView from './src/Components//Views/SettingsView';
 import HomeView from './src/Components/Views/HomeView';
 import ResponseView from './src/Components/Views/ResponseView';
 import MoreResponseView from './src/Components/Views/MoreResponseView';
-
+import AboutView from './src/Components/Views/AboutView';
 const Drawer = createDrawerNavigator();
 export default function App() {
+  const defaultTheme ={
+   dark:true,
+   colors:{
+    backgroundColor: 'black'
+   }
+  }
   return (
     <>
-    <NavigationContainer>
+    <NavigationContainer theme={defaultTheme}>
      
       <Drawer.Navigator initialRouteName="Home" backBehavior="history"
-      
+  
       screenOptions={{
         headerTitleAlign: 'center',
         headerTintColor:'white',
@@ -28,11 +36,13 @@ export default function App() {
         drawerInactiveTintColor:'white',
         drawerActiveTintColor:'#E94057',
       }
-      }>
+      }
+      >
         
         <Drawer.Screen name="Home" component={HomeView} options={{title:'Inicio'}}/>
         <Drawer.Screen  name="History" component={HistoryView} options={{title:'Historial de Busqueda',unmountOnBlur:true}}/>
         <Drawer.Screen name="Settings" component={SettingsView} options={{title:'Configuración',unmountOnBlur:true}}/>
+        <Drawer.Screen name="About" component={AboutView} options={{title:'Acerca de',unmountOnBlur:true}}/>
         <Drawer.Screen name="Response" component={ResponseView} options={{title:'Resultado',drawerLabel: '',drawerItemStyle:{width: 0},unmountOnBlur:true}}/>
         <Drawer.Screen name="MoreResponse" component={MoreResponseView} options={{title:'Más Resultados',drawerLabel: '',drawerItemStyle:{width: 0},unmountOnBlur:true}}/>
       </Drawer.Navigator>
